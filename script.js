e860cdc11c086f92b6baaa90436c9a6b948f6cf9
Tim Gabrikowski <tim@gabrikowski.de>
2023-08-31 20:55:04 +0200

    lose detection and color for only valid options

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function elem(id) {
 	return document.getElementById(id);
 }
+
+let lostBox = elem("lose_box");
+let retryBtn = elem("retry_btn");
+
+retryBtn.onclick = () => {
+	initGame();
+};
+
 class NumberGenerator {
 	/**
 	 *
@@ -8,13 +16,11 @@ class NumberGenerator {
 	 * @param {number} max Biggest number to get
 	 * @param {string} did ID of the Display element
 	 */
-	constructor(min, max, did, bid) {
+	constructor(min, max, did) {
 		this.display = elem(did);
 		this.number = 0;
 		this.minValue = min;
 		this.maxValue = max;
-
-		this.generate();
 	}
 
 	getRandomInt() {
@@ -27,6 +33,19 @@ class NumberGenerator {
 		let num = this.getRandomInt();
 		this.number = num;
 		this.display.innerText = num;
+
+		let pa = false;
+		for (let i = 0; i < 20; i++) {
+			if (canPlaceHere(num, i)) {
+				GAME_STATE.fields[i].field.style.color = "green";
+				pa = true;
+			} else {
+				GAME_STATE.fields[i].field.style.color = "red";
+			}
+		}
+		if (!pa) {
+			lostBox.style.display = "unset";
+		}
 	}
 	get() {
 		return this.number;
@@ -34,31 +53,59 @@ class NumberGenerator {
 }
 
 let GAME_STATE = {
-	generator: new NumberGenerator(
-		1,
-		999,
-		"number_display",
-		"number_gen_activate"
-	),
+	generator: new NumberGenerator(1, 999, "number_display"),
 	fields: [],
 };
+function initGame() {
+	GAME_STATE.fields = [];
+	lostBox.style.display = "none";
 
-for (let i = 1; i <= 20; i++) {
-	let e = elem("field_" + i);
-	let f = elem("num_" + i);
+	for (let i = 1; i <= 20; i++) {
+		let e = elem("field_" + i);
+		let f = elem("num_" + i);
 
-	e.onclick = () => {
-		placeOnField(i - 1);
-	};
+		e.onclick = () => {
+			placeOnField(i - 1);
+		};
 
-	GAME_STATE.fields.push({ field: e, value: null, text: f });
+		f.innerText = "---";
+
+		GAME_STATE.fields.push({ field: e, value: null, text: f });
+	}
+	GAME_STATE.generator.generate();
 }
+initGame();
 
 function placeOnField(id) {
 	let currentNum = GAME_STATE.generator.get();
+
+	if (!canPlaceHere(currentNum, id)) return;
+
 	GAME_STATE.fields[id].text.innerText = currentNum;
 	GAME_STATE.fields[id].value = currentNum;
 	GAME_STATE.fields[id].field.onclick = () => {};
 
 	GAME_STATE.generator.generate();
+}
+
+function canPlaceHere(num, id) {
+	let ok = true;
+	if (GAME_STATE.fields[id].value !== null) return false;
+	for (let i = 0; i < id; i++) {
+		const field = GAME_STATE.fields[i];
+		if (field.value !== null) {
+			if (field.value > num) {
+				ok = false;
+			}
+		}
+	}
+	for (let i = id; i < GAME_STATE.fields.length; i++) {
+		const field = GAME_STATE.fields[i];
+		if (field.value !== null) {
+			if (field.value < num) {
+				ok = false;
+			}
+		}
+	}
+	return ok;
 }
